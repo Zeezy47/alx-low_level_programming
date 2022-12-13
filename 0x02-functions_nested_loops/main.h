728d0dd630111cce255c613363a562a52d6f6013
Ochiogu Chisom <ochiogu.chisom@gmail.com>
2022-12-13 08:22:16 -0800

    print _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 #define HOLBERTON_H_INCLUDED
 
 
-int _putchar(char);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -13,4 +13,7 @@ int print_last_digit(int);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
+void num(int r, int n);
 
+#endif
