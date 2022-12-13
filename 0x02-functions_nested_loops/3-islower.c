5b9cc5c639b429911208ab0da57e1ccf230d74ba
Ochiogu Chisom <ochiogu.chisom@gmail.com>
2022-12-13 15:00:02 -0800

    check for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - check if char is lowercase
+ * Return: 1 if chzr is lowercase, else 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z.)
+		return (1);
+	else
+		return (0);
+}
