1ad091ba41fefbe4948788410563272afbb6f494
Ochiogu Chisom <ochiogu.chisom@gmail.com>
2022-12-13 09:11:46 -0800

    print alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,12 +9,14 @@
 void print_alphabet_x10(void)
 {
 	char c, i;
+
 	for (i = 0; 1 <= 9; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
+	return (0);
 }
