1647952ccc842b9eed89b8f77737b9a6863c9890
Ochiogu Chisom <ochiogu.chisom@gmail.com>
2022-12-13 16:59:07 -0800

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - command to print last digit
+ * @n: the number
+ * Return: value of the number
+ */
+
+int print_last_digit(int n)
+{
+	int ld = n % 10;
+
+	if (ld < 0)
+		ld *= -1;
+
+	_putchart(ld + '10');
+
+	return (0);
+}
