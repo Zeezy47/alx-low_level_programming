ea1d01b7d4fc1a94e5cff7cdd9110347b79ed77e
Ochiogu Chisom <ochiogu.chisom@gmail.com>
2022-12-13 16:01:59 -0800

    print sign of number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - print number sign
+ * @n: number to be checked
+ * Return: 1 and print + if n is greater than zero
+ * print 0 if n is greater than 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+	_putchar('_');
+	return (-1);
+)
