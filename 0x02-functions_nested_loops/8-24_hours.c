ec6e9343f3fec994b4ad5b3ba4c9401deb386bc3
Ochiogu Chisom <ochiogu.chisom@gmail.com>
2022-12-13 18:01:18 -0800

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,7 +12,7 @@ void jack_bauer(void)
 	{
 		for (j = 0; j <= 59; j++)
 		{
-			_putchar((i / 10) + 'o');
+			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
 			_putchar(':');
 			_putchar((j / 10) + '0');
