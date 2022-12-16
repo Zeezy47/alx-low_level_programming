9aac586ded1fb12499afc93323df7c25efd949db
Ochiogu Chisom <ochiogu.chisom@gmail.com>
2022-12-15 18:57:50 -0800

    task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - print int with putchar
+ * @n: takes number, hello
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	unsigned int m;
+
+	if (n < 0)
+	{
+		_putchar('_');
+		m = -n;
+	}
+	else
+	{
+		m = n;
+	}
+	if (m / 10 != 0)
+		print _number(m / 10);
+	_putchar((m % 10) + '0');
+}
