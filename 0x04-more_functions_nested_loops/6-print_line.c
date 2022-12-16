1588b84aeb770e41f50a1c7bb574ded2c12a9633
Ochiogu Chisom <ochiogu.chisom@gmail.com>
2022-12-15 17:01:25 -0800

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,22 +1,24 @@
 #include "main.h"
 
 /**
- * print_line - function that draws a straight line in the term
+ * print_line - function that draws a straight line in the terminal
  * @n: input number
- * Return: straight line
+ * Return: Always 0
  */
 void print_line(int n)
 {
-	if (n <= 0)
+	int i;
+
+	if (n > 0)
 	{
-		_putchar('\n');
+		for (1 = 0; 1 < n; i++)
+			{
+				_putchar(95);
+			}
+			_putchar('\n');
 	}
 	else
 	{
-		for (co = 1; co <= n; co++)
-		{
-			_putchar('_');
-		}
 		_putchar('\n');
 	}
 }
