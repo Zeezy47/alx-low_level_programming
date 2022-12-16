e1538ce387386ac7a3f6228cb8a1cf52d221bfec
Ochiogu Chisom <ochiogu.chisom@gmail.com>
2022-12-15 17:50:59 -0800

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+int i;
+char f[] = "Fizz";
+char b[] = "Buzz";
+char fb[] = "FizzBuzz";
+
+for (i = 1; i <= 100; i++)
+{
+	if (i == 100)
+	{
+		printf("%s", b);
+	}
+
+	else if ((i % 3 == 0) && (i % 5 == 0))
+	{
+		printf("%s ", fb);
+	}
+
+	else if (i % 5 == 0)
+	{
+		printf("%s ", b);
+	}
+	else if (i % 3 == 0)
+	{
+		printf("%s ", f);
+	}
+	else
+	{
+		printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
