1a9e9a8b7faaf67a622f48bd0329b7cac0dba3c2
Ochiogu Chisom <ochiogu.chisom@gmail.com>
2023-01-02 17:50:54 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,11 +11,11 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int a = 0;
 	int b = n;
-	
-	for(; a < b; a++)
+
+	for (; a < b; a++)
 	{
 		dest[a] = src[a];
 		n--;
 	}
-		return (dest);
+	return (dest);
 }
