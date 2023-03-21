2d26c2d877361afad1a4bd9ce9fe5828ede02e23
Ochiogu <iamcheesom@gmail.com>
2023-03-20 20:34:26 -0700

    0x18. C - Dynamic libraries

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - computes the abs of an int
+ * @n: the init in question
+ * Return: the abs of the int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
