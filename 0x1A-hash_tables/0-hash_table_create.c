14d3272d54e71cf77eb972f5f890825b6bd361fc
Ochiogu <iamcheesom@gmail.com>
2023-04-07 05:26:49 -0700

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,25 +8,21 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned int i = 0;
-	hash_table_t *ht = malloc(sizeof(hash_table_t));
+	unsigned long int i;
+	hash_table_t *ht;
 
+	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 	{
-		fprintf(stderr, "Error: malloc failed\n");
 		return (NULL);
 	}
-
 	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
 	{
-		fprintf(stderr, "Error: malloc failed\n");
 		return (NULL);
 	}
-
-	for (; i < size; i++)
+	for (i = 0; i < size; i++)
 		ht->array[i] = NULL;
-
 	return (ht);
 }
